c9f0f317f8d6f805c87fa32f10dc454e6d52c2e0
Mate Toth <mztbusinessenquiries@gmail.com>
2024-04-03 09:24:50 +0100

    login page code done

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
 import express from 'express';
 import dotenv from 'dotenv';
+import User from './Models/User.js';
 dotenv.config();
 const app = express();
+app.use(express.json());
 
 mongoose.connect(process.env.URI).then(() => {
   console.log('Connected to MongoDB!');
@@ -12,6 +14,47 @@ mongoose.connect(process.env.URI).then(() => {
       `Backend server running on http://localhost:${process.env.PORT}`,
     ),
   );
+});
+
+const createUser = async (name, dob, email, password) => {
+  const user = await User.create({
+    name: name,
+    dob: dob,
+    email: email,
+    password: password,
+  });
+  return user;
+};
+
+app.post('/api/users/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const foundUser = await User.findOne({ email: email, password: password });
+    if (!foundUser) {
+      return res
+        .status(404)
+        .json({ message: 'No account was found matching the email' });
+    }
+    return res.status(200).json(foundUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Something went wrong on our end' });
+  }
+});
+
+app.post('/api/users', async (req, res) => {
+  try {
+    console.log(req.body);
+    const { name, dob, email, password } = req.body;
+    const user = await createUser(name, dob, email, password);
+    if (!user) {
+      throw e;
+    }
+    res.status(201).send(user);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: 'Something went wrong on our end' });
+  }
 });
 
 app.get('/', (req, res) => {
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import { MainPage } from '../pages/MainPage';
+import { LoginPage } from '../pages/LoginPage';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element=<MainPage /> />
+      <Route path='/login' element=<LoginPage /> />
     </Routes>
   </BrowserRouter>,
 );
