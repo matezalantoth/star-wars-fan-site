a4424e46eb89148e93a8bb73c0167770647a7d38
blameaimer <blameaimer@gmail.com>
2024-04-03 11:51:23 +0200

    created navbar and all the necessary pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,13 +5,22 @@ import './index.css';
 import { MainPage } from './pages/MainPage';
 import { LoginPage } from './pages/LoginPage';
 import { SignupPage } from './pages/SignupPage';
+import Navbar from './components/Navbar.jsx';
+import { CharactersPage } from './pages/CharactersPage.jsx';
+import { PlanetsPage } from './pages/PlanetsPage.jsx';
+import { VehiclesPage } from './pages/VehiclesPage.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element=<MainPage /> />
-      <Route path='/login' element=<LoginPage /> />
-      <Route path='/signup' element=<SignupPage /> />
+      <Route path='/' element={<Navbar />}>
+        <Route path='/' element=<MainPage /> />
+        <Route path='/login' element=<LoginPage /> />
+        <Route path='/signup' element=<SignupPage /> />
+        <Route path='/characters' element=<CharactersPage /> />
+        <Route path='/planets' element=<PlanetsPage /> />
+        <Route path='/vehicles' element=<VehiclesPage /> />
+      </Route>
     </Routes>
-  </BrowserRouter>,
+  </BrowserRouter>
 );
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,3 +1,32 @@
+import { useEffect, useState } from 'react';
+
 export const MainPage = () => {
-  return <div>Hello world!</div>;
+  const [movies, setMovies] = useState();
+
+  useEffect(() => {
+    const fetchMovieData = async () => {
+      const httpResponse = await fetch('/api/films');
+
+      const response = await httpResponse.json();
+
+      setMovies(response.results);
+    };
+    fetchMovieData();
+  }, []);
+
+  console.log(movies);
+
+  return (
+    <div>
+      {movies ? (
+        <ul>
+          {movies.map((movie) => {
+            return <li key={movie}>{movie.title}</li>;
+          })}
+        </ul>
+      ) : (
+        'hi'
+      )}
+    </div>
+  );
 };
