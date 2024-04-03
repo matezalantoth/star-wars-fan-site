3526b60898770ab2f2e1a773a1f5945bc7f4eae7
Mate Toth <mztbusinessenquiries@gmail.com>
2024-04-03 10:01:01 +0100

    merge conflict file issues resolved

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,32 +1,3 @@
-import { useEffect, useState } from 'react';
-
 export const MainPage = () => {
-  const [movies, setMovies] = useState();
-
-  useEffect(() => {
-    const fetchMovieData = async () => {
-      const httpResponse = await fetch('/api/films');
-
-      const response = await httpResponse.json();
-
-      setMovies(response.results);
-    };
-    fetchMovieData();
-  }, []);
-
-  console.log(movies);
-
-  return (
-    <div>
-      {movies ? (
-        <ul>
-          {movies.map((movie) => {
-            return <li key={movie}>{movie.title}</li>;
-          })}
-        </ul>
-      ) : (
-        'hi'
-      )}
-    </div>
-  );
+  return <div>Hello world!</div>;
 };
