ea31bda82199f1e7e699bdeae80bcd901e591399
blameaimer <blameaimer@gmail.com>
2024-04-16 10:39:57 +0200

    Created responsive navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,22 @@
 /* eslint-disable react/prop-types */
 
+import { Fragment } from 'react';
+import { Disclosure, Menu, Transition } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { useState } from 'react';
-import { Link, Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import logo from '../assets/Star_Wars_Logo.svg.png';
+
+const navigation = [
+  { name: 'Films', href: '/', current: true },
+  { name: 'Characters', href: '/characters', current: false },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
 
 export default function Navbar(props) {
   // eslint-disable-next-line react/prop-types
@@ -12,72 +24,117 @@ export default function Navbar(props) {
   const navigate = useNavigate();
 
   const { cookies, setUserLoginCookies } = props;
+
   return (
     <>
-      <nav className='bg-black'>
-        <div className='relative flex h-16 w-screen'>
-          <div className='flex sm:items-stretch sm:justify-start'>
-            <img
-              className='h-8 ml-6 mt-6 w-auto'
-              src={logo}
-              alt='Your Company'
-            />
-            <div className='hidden sm:ml-6 sm:block'>
-              <div className='flex space-x-4'>
-                <ul className='inline-flex float-left'>
-                  <li className='relative top-5 right-5 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium'>
-                    <Link to='/'>Films</Link>
-                  </li>
-                  <li className='relative top-5 right-5 text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium'>
-                    <Link to='/characters'>Characters</Link>
-                  </li>
-                </ul>
+      <Disclosure as='nav' className='bg-black'>
+        {({ open }) => (
+          <>
+            <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
+              <div className='relative flex h-16 items-center justify-between'>
+                <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
+                  {/* Mobile menu button*/}
+                  <Disclosure.Button className='relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
+                    <span className='absolute -inset-0.5' />
+                    <span className='sr-only'>Open main menu</span>
+                    {open ? (
+                      <XMarkIcon className='block h-6 w-6' aria-hidden='true' />
+                    ) : (
+                      <Bars3Icon className='block h-6 w-6' aria-hidden='true' />
+                    )}
+                  </Disclosure.Button>
+                </div>
+                <div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
+                  <div className='flex flex-shrink-0 items-center'>
+                    <img className='h-8 w-auto' src={logo} alt='Your Company' />
+                  </div>
+                  <div className='hidden sm:ml-6 sm:block'>
+                    <div className='flex space-x-4'>
+                      {navigation.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className={classNames(
+                            'text-gray-300 hover:bg-gray-700 hover:text-white',
+                            'rounded-md px-3 py-2 text-sm font-medium'
+                          )}
+                          aria-current={item.current ? 'page' : undefined}>
+                          {item.name}
+                        </a>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+                <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
+                  {/* Profile dropdown */}
+                  <Menu as='div' className='relative ml-3'>
+                    <div>
+                      <Menu.Button className='relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
+                        <span className='absolute -inset-1.5' />
+                        <span className='sr-only'>Open user menu</span>
+                        <FontAwesomeIcon icon={faUser} />
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter='transition ease-out duration-100'
+                      enterFrom='transform opacity-0 scale-95'
+                      enterTo='transform opacity-100 scale-100'
+                      leave='transition ease-in duration-75'
+                      leaveFrom='transform opacity-100 scale-100'
+                      leaveTo='transform opacity-0 scale-95'>
+                      <Menu.Items className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href={cookies.user ? '/profile' : '/login'}
+                              className={classNames(
+                                active ? 'bg-gray-100' : '',
+                                'block px-4 py-2 text-sm text-gray-700'
+                              )}>
+                              Your Profile
+                            </a>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href='/login'
+                              className={classNames(
+                                active ? 'bg-gray-100' : '',
+                                'block px-4 py-2 text-sm text-gray-700'
+                              )}>
+                              {cookies.users ? 'Sign out' : 'Sign in'}
+                            </a>
+                          )}
+                        </Menu.Item>
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                </div>
               </div>
             </div>
-            <div className='absolute right-7 top-5'>
-              <button
-                onClick={() => {
-                  menuOpen ? setMenuOpen(false) : setMenuOpen(true);
-                }}
-                type='button'
-                className='rounded-md bg-white text-sm w-9 h-9 text-gray-900 hover:bg-gray-200'
-              >
-                <FontAwesomeIcon icon={faUser} />
-              </button>
-            </div>
-            <div
-              className='absolute z-10 right-7 top-14 w-36 rounded-md bg-white'
-              hidden={!menuOpen}
-            >
-              <div className='py-1' role='none'>
-                <Link
-                  to={cookies.user ? '/profile' : '/login'}
-                  className='text-gray-700 block px-4 py-2 text-sm'
-                >
-                  Profile
-                </Link>
-                <hr />
-                {cookies.user ? (
-                  <button
-                    onClick={() => {
-                      setMenuOpen(false);
-                      setUserLoginCookies(null);
-                      navigate('/login');
-                    }}
-                    className='text-gray-700 block w-full px-4 py-2 text-left text-sm'
-                  >
-                    Sign out
-                  </button>
-                ) : (
-                  <button className='text-gray-700 block w-full px-4 py-2 text-left text-sm'>
-                    <Link to='/login'>Login</Link>
-                  </button>
-                )}
+
+            <Disclosure.Panel className='sm:hidden'>
+              <div className='space-y-1 px-2 pb-3 pt-2'>
+                {navigation.map((item) => (
+                  <Disclosure.Button
+                    key={item.name}
+                    as='a'
+                    href={item.href}
+                    className={classNames(
+                      'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      'block rounded-md px-3 py-2 text-base font-medium'
+                    )}
+                    aria-current={item.current ? 'page' : undefined}>
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
               </div>
-            </div>
-          </div>
-        </div>
-      </nav>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
       <Outlet />
     </>
   );
